ec5b62d95d8d5c3ac4a7ffa766006eb095145970
zeusro <692486802@qq.com>
2020-03-07 00:54:59 +0800

    Sat Mar  7 00:54:59 CST 2020

--- sdk/aliyun/vpc.go
+++ sdk/aliyun/vpc.go
@@ -8,9 +8,12 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// 共享带宽相关接口
+// https://help.aliyun.com/document_detail/55928.html?spm=a2c4g.11186623.6.572.545b7579DjZDj2
+
 // GetPublicIpAddresseFromCommonBandwidthPackages 从第一个共享带宽里面提取EIP信息
 func (sdk *AliyunSDK) GetPublicIpAddresseFromCommonBandwidthPackages() (eipList []vpc.PublicIpAddresse, err error) {
-	client, err := vpc.NewClientWithAccessKey(sdk.config.Region, sdk.config.AccessKeyId, sdk.config.AccessSecret)
+	client := sdk.GetClient()
 	request := vpc.CreateDescribeCommonBandwidthPackagesRequest()
 	request.Scheme = "https"
 	request.PageSize = "50"
@@ -29,4 +32,34 @@ func (sdk *AliyunSDK) GetPublicIpAddresseFromCommonBandwidthPackages() (eipList 
 		commonBandwidthPackage.BandwidthPackageId)
 	eipList = commonBandwidthPackage.PublicIpAddresses.PublicIpAddresse
 	return eipList, nil
+}
+
+func (sdk *AliyunSDK) AddCommonBandwidthPackageIp(bandwidthPackageId string, ipInstanceId string) bool {
+	client := sdk.GetClient()
+	request := vpc.CreateAddCommonBandwidthPackageIpRequest()
+	request.Scheme = "https"
+	request.RegionId = sdk.config.Region
+	request.BandwidthPackageId = bandwidthPackageId
+	request.IpInstanceId = ipInstanceId
+	response, err := client.AddCommonBandwidthPackageIp(request)
+	if err != nil {
+		log.Err(err)
+		return false
+	}
+	return response.IsSuccess()
+}
+
+func (sdk *AliyunSDK) RemoveCommonBandwidthPackageIp(bandwidthPackageId string, ipInstanceId string) bool {
+	client := sdk.GetClient()
+	request := vpc.CreateRemoveCommonBandwidthPackageIpRequest()
+	request.Scheme = "https"
+	request.RegionId = sdk.config.Region
+	request.BandwidthPackageId = bandwidthPackageId
+	request.IpInstanceId = ipInstanceId
+	response, err := client.RemoveCommonBandwidthPackageIp(request)
+	if err != nil {
+		log.Err(err)
+		return false
+	}
+	return response.IsSuccess()
 }
