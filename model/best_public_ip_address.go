cd7a331a578847da243f1757faa99048ff9a8dbc
zeusro <692486802@qq.com>
2020-03-10 07:17:51 +0800

    Tue Mar 10 07:17:51 CST 2020

--- model/best_public_ip_address.go
+++ model/best_public_ip_address.go
@@ -1,6 +1,9 @@
 package model
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // BestPublicIpAddress 应用动态规划,寻求最佳EIP列表
 // https://blog.csdn.net/runbat/article/details/94016554
@@ -15,7 +18,7 @@ type BestPublicIpAddress struct {
 	// 带宽偏移量,默认值10
 	// offset int
 	//动态规划网格，网格的元素为当前局部带宽最优解
-	cellsMesh [X][Y]float64
+	cellsMesh [Y][X]float64
 	// 带宽(类似于背包问题的物品容量)
 	bandwidths []float64
 }
@@ -27,43 +30,64 @@ const (
 	// DEFAULT_OFFSET=10
 )
 
-func NewBestPublicIpAddress(minBandwidth int, maxBandwidth int) *BestPublicIpAddress {
-	return &BestPublicIpAddress{
-		minBandwidth: minBandwidth,
+func NewBestPublicIpAddress(minBandwidth int, bandwidthInfos []EipAvgBandwidthInfo) (*BestPublicIpAddress, error) {
+	if len(bandwidthInfos) > Y {
+		err := errors.New("超过了可解范围")
+		return nil, err
 	}
+	bestIPs := &BestPublicIpAddress{
+		minBandwidth: minBandwidth,
+		origin:       bandwidthInfos,
+	}
+	return bestIPs, nil
 }
 
-func (m *BestPublicIpAddress) FindBestPublicIpAddress() {
-
+func (m *BestPublicIpAddress) FindBestPublicIpAddress() []EipAvgBandwidthInfo {
+	m.dynamic()
+	return m.FindBestPublicIpAddress()
 }
 
 func (m *BestPublicIpAddress) dynamic() {
 	listLen := len(m.origin)
 	// 初始化动态规划网格
-	for i := 1; i < listLen; i++ {
-		for j := 1; j < Y; j++ {
-			m.cellsMesh[i][j] = m.maxValue(i, j)
+	for y := 1; y < listLen; y++ {
+		for x := 1; x < X; x++ {
+			m.cellsMesh[y][x] = m.maxValue(y, x)
 		}
 	}
-	for i := 0; i < Y; i++ {
-		fmt.Printf("%v \n", m.cellsMesh[i])
+	for y := 0; y < listLen; y++ {
+		fmt.Printf("%v \n", m.cellsMesh[y])
 	}
 
 }
 
 // 局部最优解
-func (m *BestPublicIpAddress) maxValue(i, j int) float64 {
+func (m *BestPublicIpAddress) maxValue(y, x int) float64 {
 	// 当前商品无法放入背包，返回当前背包所能容纳的最大价值
-	currentMaxBandwidth := float64(j + m.minBandwidth)
-	var getValue = func(currentBandwidth int) float64 {
-		// 离最小带宽越近越好,所以以负数形式返回
-		return float64(m.minBandwidth - currentBandwidth)
-	}
-	if m.origin[i].Value > currentMaxBandwidth {
-		return m.cellsMesh[i-1][j]
+	currentMaxBandwidth := float64(x + m.minBandwidth)
+	if m.origin[y].Value > currentMaxBandwidth {
+		return m.cellsMesh[y-1][x]
 	}
 	// 可放进背包时候，计算放入当前商品后的最大价值
-	currentValue := getValue(i+m.minBandwidth) + m.cellsMesh[i-1][j-m.bandwidths[i]]
+	// 每一个EIP的价值都是1
+	// 当前价值= EIP价值 + 剩余空间的价值
+	// 剩余空间的价值=cell[i-1][j-当前商品的重量])
+	currentValue := 1 + m.cellsMesh[y-1][x-1]
+	if currentValue >= m.cellsMesh[y-1][x] {
+		return currentValue
+	}
+	return m.cellsMesh[y-1][x]
+}
 
-	return 0
+// 回溯选择的商品方法
+func (m *BestPublicIpAddress) findBack() []EipAvgBandwidthInfo {
+	col := X - 1
+	for i := Y - 1; i > 0; i-- {
+		if m.cellsMesh[i][col] > m.cellsMesh[i-1][col] {
+			// selected[i] = 1
+			m.best = append(m.best, m.origin[i])
+			col = col - 1
+		}
+	}
+	return m.best
 }
